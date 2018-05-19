77c98c5c231de3c2cf46296db0f1b2ee4c0cecfd
Kay Hoang <katehoang36@gmail.com>
2018-05-20 00:32:08 +1000

    fix membership crash problem

--- On-The-House/Controller/MyMembershipViewController.swift
+++ On-The-House/Controller/MyMembershipViewController.swift
@@ -11,19 +11,38 @@ import SwiftyJSON
 
 class MyMembershipViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableViewMembershipHistory: UITableView!
-    var membership :[String: Any]?
-    var membershipHistory :[[String: Any]]?
-    var userFullName:String?
+    var memberID:String?
+    var membershipHistory :[[String: Any]] = [[:]]
     @IBOutlet weak var lbUserFullname: UILabel!
     @IBOutlet weak var lbCurrentMembership: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        lbCurrentMembership.text = UserDefaults.standard.string(forKey:"membership_level_name")! + " Member"
-        lbUserFullname.text = userFullName
-        
         tableViewMembershipHistory.dataSource = self
         tableViewMembershipHistory.delegate = self
+        self.memberID = UserDefaults.standard.string(forKey: "member_id")!
+        lbCurrentMembership.text = UserDefaults.standard.string(forKey:"membership_level_name")! + " Member"
+        lbUserFullname.text = UserDefaults.standard.string(forKey:"first_name")! + " " + UserDefaults.standard.string(forKey:"last_name")!
+        loadMembershipHistory()
     }
+    func loadMembershipHistory(){
+        let postBodys = "member_id=\(memberID!)"
+        if let memberURL = URL(string: "http://ma.on-the-house.org/api/v1/member/membership/history"){
+            let network = NetworkProcessor(url: memberURL)
+            network.PostJSONFromURL(postString: postBodys) { (jsonDictionary) in
+                if let status = jsonDictionary?["status"] as? String{
+                    if status == "success"{
+                        self.membershipHistory = (jsonDictionary?["memberships"] as? [[String: Any]])!
+                        DispatchQueue.main.async{
+                            self.tableViewMembershipHistory.reloadData()
+                        }
+                    }else if status == "error"{
+                        print("fail to load json")
+                    }
+                }
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,7 +52,8 @@ class MyMembershipViewController: UIViewController, UITableViewDelegate, UITable
         return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.membershipHistory!.count
+        print(self.membershipHistory.count)
+        return self.membershipHistory.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -41,20 +61,23 @@ class MyMembershipViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         tableViewMembershipHistory.dataSource = self
         let cell = tableViewMembershipHistory.dequeueReusableCell(withIdentifier: "myMemHis", for: indexPath) as! MyMembershipHistoryTableViewCell
-        cell.lbMembershipName.text = membershipHistory![indexPath.row]["membership_level_name"] as? String
+        cell.lbMembershipName.text = membershipHistory[indexPath.row]["membership_level_name"] as? String
         //convert date format
-        let startDateAsString = membershipHistory![indexPath.row]["date_created"] as! String
-        let endDateAsString = membershipHistory![indexPath.row]["date_expires"] as! String
-        if let startDateAsDouble = Double(startDateAsString), let endDateAsDouble = Double(endDateAsString){
+        let startDateAsString = membershipHistory[indexPath.row]["date_created"] as? String
+        let endDateAsString = membershipHistory[indexPath.row]["date_expires"] as? String
+        if let startDateAsDouble = startDateAsString?.toDouble(), let endDateAsDouble = endDateAsString?.toDouble(){
             let startDate:String = DateFormat.getFormattedDate(dateToConvert: startDateAsDouble, format: "dd/MM/YYYY")
             let endDate:String = DateFormat.getFormattedDate(dateToConvert: endDateAsDouble, format: "dd/MM/YYYY")
             cell.lbDuration.text = startDate + " - " + endDate
         }
         return cell
     }
-    
+}
+extension String {
+    func toDouble() -> Double? {
+        return NumberFormatter().number(from: self)?.doubleValue
+    }
 }
 
