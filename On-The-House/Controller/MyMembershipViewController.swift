4982074cf41a9830bd6fd1e7b97d5911550bcd53
Kay Hoang <katehoang36@gmail.com>
2018-05-22 15:35:29 +1000

    Revert "make upgrade membership payment with paypal"
    This reverts commit 07d9cac9814b958a0e2871a6a90f4214042b9af9.

--- On-The-House/Controller/MyMembershipViewController.swift
+++ On-The-House/Controller/MyMembershipViewController.swift
@@ -9,62 +9,21 @@
 import UIKit
 import SwiftyJSON
 
-class MyMembershipViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, PayPalPaymentDelegate {
-    @IBOutlet weak var upgradeMembershipBtn: UIButton!
+class MyMembershipViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableViewMembershipHistory: UITableView!
     var memberID:String?
     var membershipHistory :[[String: Any]] = [[:]]
     @IBOutlet weak var lbUserFullname: UILabel!
     @IBOutlet weak var lbCurrentMembership: UILabel!
-    var environment:String = PayPalEnvironmentNoNetwork {
-        willSet(newEnvironment) {
-            if (newEnvironment != environment) {
-                PayPalMobile.preconnect(withEnvironment: newEnvironment)
-            }
-        }
-    }
-    var payPalConfig = PayPalConfiguration() // default
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkMembershipStatus()
         tableViewMembershipHistory.dataSource = self
         tableViewMembershipHistory.delegate = self
         self.memberID = UserDefaults.standard.string(forKey: "member_id")!
         lbCurrentMembership.text = UserDefaults.standard.string(forKey:"membership_level_name")! + " Member"
         lbUserFullname.text = UserDefaults.standard.string(forKey:"first_name")! + " " + UserDefaults.standard.string(forKey:"last_name")!
         loadMembershipHistory()
-        
-        // Set up payPalConfig
-        payPalConfig.acceptCreditCards = false
-        payPalConfig.merchantName = "On The House, Inc."
-        payPalConfig.merchantPrivacyPolicyURL = URL(string: "https://www.paypal.com/webapps/mpp/ua/privacy-full")
-        payPalConfig.merchantUserAgreementURL = URL(string: "https://www.paypal.com/webapps/mpp/ua/useragreement-full")
-        // Setting the languageOrLocale property is optional.
-        //
-        // If you do not set languageOrLocale, then the PayPalPaymentViewController will present
-        // its user interface according to the device's current language setting.
-        //
-        // Setting languageOrLocale to a particular language (e.g., @"es" for Spanish) or
-        // locale (e.g., @"es_MX" for Mexican Spanish) forces the PayPalPaymentViewController
-        // to use that language/locale.
-        //
-        // For full details, including a list of available languages and locales, see PayPalPaymentViewController.h.
-        
-        payPalConfig.languageOrLocale = Locale.preferredLanguages[0]
-        // Setting the payPalShippingAddressOption property is optional.
-        //
-        // See PayPalConfiguration.h for details.
-        
-        payPalConfig.payPalShippingAddressOption = .payPal;
-        
-        print("PayPal iOS SDK Version: \(PayPalMobile.libraryVersion())")
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        PayPalMobile.preconnect(withEnvironment: environment)
-    }
-    
     func loadMembershipHistory(){
         let postBodys = "member_id=\(memberID!)"
         if let memberURL = URL(string: "http://ma.on-the-house.org/api/v1/member/membership/history"){
@@ -114,68 +73,6 @@ class MyMembershipViewController: UIViewController, UITableViewDelegate, UITable
             cell.lbDuration.text = startDate + " - " + endDate
         }
         return cell
-    }
-    @IBAction func UpgradeMembershipAction(_ sender: UIButton) {
-        let payment = PayPalPayment(amount: 0.1, currencyCode: "AUD", shortDescription: "Upgrade to gold membership", intent: .sale)
-        
-        if (payment.processable) {
-            let paymentViewController = PayPalPaymentViewController(payment: payment, configuration: payPalConfig, delegate: self)
-            present(paymentViewController!, animated: true, completion: nil)
-        }
-        else {
-            // This particular payment will always be processable. If, for
-            // example, the amount was negative or the shortDescription was
-            // empty, this payment wouldn't be processable, and you'd want
-            // to handle that here.
-            print("Payment not processalbe: \(payment)")
-        }
-    }
-    
-    // PayPalPaymentDelegate
-    
-    func payPalPaymentDidCancel(_ paymentViewController: PayPalPaymentViewController) {
-        print("PayPal Payment Cancelled")
-        paymentViewController.dismiss(animated: true, completion: nil)
-    }
-    
-    func payPalPaymentViewController(_ paymentViewController: PayPalPaymentViewController, didComplete completedPayment: PayPalPayment) {
-        print("PayPal Payment Success !")
-        paymentViewController.dismiss(animated: true, completion: { () -> Void in
-            // send completed confirmaion to your server
-            print("Here is your proof of payment:\n\n\(completedPayment.confirmation)\n\nSend this to your server for confirmation and fulfillment.")
-            //update user membership in app and in the UI
-            UserDefaults.standard.set("9", forKey: "membership_level_id")
-            UserDefaults.standard.set("Gold", forKey: "membership_level_name")
-            self.upgradeMembershipBtn.isHidden = true
-            self.lbCurrentMembership.text = UserDefaults.standard.string(forKey:"membership_level_name")! + " Member"
-            //send upgrade information to server
-            self.sendUpgradeInformation()
-        })
-    }
-    
-    func sendUpgradeInformation(){
-        let memberID = UserDefaults.standard.string(forKey: "member_id")
-        let memberShipLevelID = "9"  //gold membership level id is 9
-        let postBodys = "member_id=\(memberID!)&membership_level_id=\(memberShipLevelID)"
-        if let memberURL = URL(string: "http://ma.on-the-house.org/api/v1/member/membership/update"){
-            let network = NetworkProcessor(url: memberURL)
-            network.PostJSONFromURL(postString: postBodys) { (jsonDictionary) in
-                if let status = jsonDictionary?["status"] as? String{
-                    if status == "success"{
-                        print("Successfull Upgrade your membership!")
-                    }else if status == "error"{
-                        print("fail to send confirmation information")
-                    }
-                }
-            }
-        }
-    }
-    
-    //If membership is gold then upgrade button will be hidden
-    func checkMembershipStatus(){
-        if (UserDefaults.standard.string(forKey: "membership_level_id")! == "9"){
-            upgradeMembershipBtn.isHidden = true
-        }
     }
 }
 extension String {
